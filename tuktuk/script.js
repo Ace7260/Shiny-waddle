6df6badb6a5f4d426c05900ab29e366f41f0b532
Ace7260 <irano7260@gmail.com>
2024-01-28 09:45:26 +0200

    tuktuk

---
+++ tuktuk/script.js
@@ -0,0 +1,11 @@
+// loader = document.querySelector(".loader");
+// // main = document.querySelector(".main");
+
+// function init() {
+//     setTimeout(() => {
+//         loader.style.display = "none";
+//         // main.style.display = 'block';
+//     }, 4000);
+// }
+// init();
+// fetch("E:\\Programmation Web\\app.json")
